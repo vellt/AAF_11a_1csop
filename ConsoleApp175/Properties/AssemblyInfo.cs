c7a0de92909e868d3c720193eab454501a4c9ca4
vellt <szantobenjamin97@gmail.com>
2025-04-08 20:19:29 +0200

    mai ora

--- ConsoleApp175/Properties/AssemblyInfo.cs
+++ ConsoleApp175/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp173")]
+[assembly: AssemblyTitle("ConsoleApp175")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp173")]
+[assembly: AssemblyProduct("ConsoleApp175")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b56ba852-4245-475a-9302-b5b95a46bd58")]
+[assembly: Guid("c1338312-c748-4453-b90b-1a3e18967585")]
 
 // Version information for an assembly consists of the following four values:
 //
