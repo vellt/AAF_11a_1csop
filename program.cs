ed5bd898f4e7fc62363b29027ec29810b2ecd7cf
vellt <szantobenjamin97@gmail.com>
2025-05-13 15:04:14 +0200

    mai ora

---
+++ program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // csupa i betűs
+            Console.Write("Adj meg egy szöveget: ");
+            string szoveg = Console.ReadLine();
+            string iBetus = "";
+
+            //Console.WriteLine(szoveg);
+
+            for (int i = 0; i < szoveg.Length; i++)
+            {
+                char karakter = szoveg[i];
+
+                if (karakter == 'a' || karakter == 'á' || karakter == 'e' || karakter == 'é' || karakter == 'o' || karakter == 'ó' || karakter == 'ö' || karakter == 'ő' || karakter == 'u' || karakter == 'ú' || karakter == 'ü' || karakter == 'ű')
+                {
+                    iBetus += 'i';
+                }
+                else
+                {
+                    iBetus += karakter;
+                }
+            }
+
+            Console.WriteLine($"{szoveg} csupa i betűvel: {iBetus}");
+            Console.ReadKey();
+        }
+    }
+}
